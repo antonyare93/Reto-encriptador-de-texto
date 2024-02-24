cc97ca6c6e115663b8d9b161885d089ddee42e15
Antony Arévalo <antonyare_93@hotmail.com>
2024-02-24 14:24:48 -0500

    bug_james_solved

--- encrypt.js
+++ encrypt.js
@@ -24,11 +24,48 @@ function valores_encriptado(letra) {
     }
 }
 
+function valores_desencriptado(letra) {
+    if (letra === "a"){
+        return ["a", 1];
+    } else if (letra === "e"){
+        return ["e", 4];
+    } else if (letra === "i"){
+        return ["i", 3];
+    } else if (letra === "o"){
+        return ["o", 3];
+    } else if (letra === "u"){
+        return ["u", 3];
+    } else {
+        return [letra, 0];
+    }
+}
+
 function encriptar() {
     let texto = document.getElementById("texto").value;
     let textoEncriptado = ""
     for (let i = 0; i < texto.length; i++) {
         textoEncriptado += valores_encriptado(texto[i]);
     }
-    printResult("<p>" + textoEncriptado + "</p>");
+    printResult(textoEncriptado);
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    /* let textoDesencriptado = texto.replaceAll("enter", "e").replaceAll("ufat", "u").
+    replaceAll("ober", "o").replaceAll("imes", "i").
+    replaceAll("ai", "a");*/
+    let textoDesencriptado = ""
+    let validacion = []
+    for (let i = 0; i < texto.length; i++) {
+        validacion = valores_desencriptado(texto[i]);
+        textoDesencriptado += validacion[0];
+        i += parseInt(validacion[1]);
+    }
+    printResult(textoDesencriptado);
+}
+
+function copyClipboard() {
+    let valor = document.getElementById("resultado").innerText;
+    navigator.clipboard.writeText(valor);
+    alert("¡Resultado copiado!");
 }
